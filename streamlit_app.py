82e2be497df71bef1f914e9e6aca0543086438dc
iandoxsee <79182067+iandoxsee@users.noreply.github.com>
2023-05-16 14:22:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,68 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
+from io import StringIO
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
+# Function to convert FASTA sequence to amino acid sequence
+def fasta_to_amino_acid(fasta_seq, wild_type):
     data = []
 
-    points_per_turn = total_points / num_turns
+    # T7 tag sequence
+    t7_tag = "MGSSHHHHHHSSGLVPRGSHMASMTGGQQMGRGSEF"
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+    # Create a sequence record from the fasta sequence
+    seq_record = SeqIO.read(StringIO(fasta_seq), "fasta", alphabet=IUPAC.unambiguous_dna)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Translate the nucleotide sequence to amino acid sequence
+    amino_acid_seq = str(seq_record.seq.translate(to_stop=False))
+
+    # Split the sequence by '*' which is the stop codon
+    proteins = amino_acid_seq.split('*')
+
+    # Loop through the proteins and add them to the list
+    for protein in proteins:
+        # Find the index of the first 'M'
+        m_index = protein.find('M')
+        if m_index != -1:
+            # If 'M' is found, add the protein from 'M' to the end to the list
+            protein = protein[m_index:]
+
+            # Check if the protein starts with the T7 tag
+            if protein.startswith(t7_tag):
+                # If it does, remove the tag and add the remaining protein to the list
+                protein = protein[len(t7_tag):]
+
+                # Compare the protein with the wild-type
+                mutations = []
+                for i in range(min(len(protein), len(wild_type))):
+                    if protein[i] != wild_type[i]:
+                        # If the amino acids don't match, record the mutation
+                        mutations.append(f'{wild_type[i]}{i+1}{protein[i]}')
+
+                data.append({"id": seq_record.id, "protein": protein, "mutations": ", ".join(mutations)})
+
+    # Create a dataframe from the list
+    df = pd.DataFrame(data)
+
+    return df
+
+# Streamlit code
+st.title('Protein Sequence Analysis')
+
+# Input for the wild-type sequence
+wild_type = st.text_area('Enter the wild-type amino acid sequence:')
+
+# Input for the FASTA sequence
+fasta_seq = st.text_area('Enter the FASTA DNA sequence:')
+
+if st.button('Analyze'):
+    if wild_type and fasta_seq:
+        # Convert FASTA sequence to amino acid sequence and compare with wild-type
+        df = fasta_to_amino_acid(fasta_seq, wild_type)
+
+        # Display the results
+        st.write(df)
+    else:
+        st.write('Please enter both the wild-type and FASTA sequences.')
